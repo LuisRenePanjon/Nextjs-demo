547bf4e9626f7aaa5027c34138fad25dc376dd55
Luis René Panjón <luchorene97@gmail.com>
2022-04-19 14:53:24 -0500

    Add title in contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,12 +1,15 @@
-import Link from "next/link";
-import { MainLayout } from "../../components/layouts/MainLayout";
-import styles from '../../styles/Home.module.css'
+import Link from 'next/link';
+import { MainLayout } from '../../components/layouts/MainLayout';
+import styles from '../../styles/Home.module.css';
 
 export default function ContactPage() {
     return (
         <MainLayout>
+            <h1 className={'text-3xl font-bold underline text-red-600'}>
+                Contact
+            </h1>
             <h1 className={styles.title}>
-                Ir a  <Link href={'/'} >index!</Link>
+                Ir a <Link href={'/'}>index!</Link>
             </h1>
 
             <p className={styles.description}>
@@ -14,5 +17,5 @@ export default function ContactPage() {
                 <code className={styles.code}>pages/contact.jsx</code>
             </p>
         </MainLayout>
-    )
+    );
 }

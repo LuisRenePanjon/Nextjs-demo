c601f9aa5e0d79e426b4b1ed5dfad7200963645b
Luis René Panjón <luchorene97@gmail.com>
2022-04-18 19:12:30 -0500

    Generate navbar options by menuItems list

--- components/commons/NavBar.jsx
+++ components/commons/NavBar.jsx
@@ -1,12 +1,31 @@
 
 import { ActiveLink } from './ActiveLink'
 
+const menuItems = [
+    {
+        text: 'Home',
+        href: '/'
+    },
+    {
+        text: 'About',
+        href: '/about'
+    },
+    {
+        text: 'Contact',
+        href: '/contact'
+    },
+    {
+        text: 'Pricing',
+        href: '/pricing'
+    },
+];
+
 export const NavBar = () => {
     return (
         <nav className="flex items-center flex-wrap bg-teal-500 p-6">
-            <ActiveLink text={"Inicio"} href={'/'} />
-            <ActiveLink text={"Acerca de"} href={'/about'} />
-            <ActiveLink text={"Contacto"} href={'/contact'} />
+            {menuItems.map((item) => (
+            <ActiveLink text={item.text} href={item.href} key={item.text} />
+            ))}
         </nav>
     )
 }
